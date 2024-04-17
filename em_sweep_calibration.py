b3a89d27d658f41c785b259473bafc6572224e80
sam <skaplan1.olin.edu>
2024-04-16 20:27:57 -0400

    add a bunch of stuff

---
+++ em_sweep_calibration.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+
+x = np.linspace(0, 255, num = 256)
+y_list = []
+
+file1 = open('em_sweep.txt', 'r')
+while True:
+    # Get next line from file
+    line = file1.readline()
+
+    # break if no new data
+    if not line:
+        break
+    y_list.append(line.rstrip('\n'))
+ 
+    
+file1.close()
+
+y = np.array(y_list)
+
+# plotting raw data
+fig, ax = plt.subplots()
+ax.plot(x,y)
+plt.xlabel("Electromagnet PWM Pulse")
+plt.ylabel("Hall Effect Sensor Raw Data")
+plt.title("Electromagnet Sweep Calibration")
+
+ax.set_yticks(ax.get_yticks()[::10])
+
+plt.show()  # display
+
+
+# curve_fit() function takes the test-function
+# x-data and y-data as argument and returns 
+# the coefficients a and b in param and
+# the estimated covariance of param in param_cov
+param, param_cov = curve_fit(x, y)
+
+
+print("Sine function coefficients:")
+print(param)
+print("Covariance of coefficients:")
+print(param_cov)
+
+# ans stores the new y-data according to 
+# the coefficients given by curve-fit() function
+ans = (param[0]*(np.sin(param[1]*x)))
+
+'''Below 4 lines can be un-commented for plotting results 
+using matplotlib as shown in the first example. '''
+
+# plt.plot(x, y, 'o', color ='red', label ="data")
+# plt.plot(x, ans, '--', color ='blue', label ="optimized data")
+# plt.legend()
+# plt.show()
--- sensor_calibration/sensor_calibration.ino
+++ sensor_calibration/sensor_calibration.ino
@@ -1,19 +1,93 @@
+#define MOTOR_IN1 10
+#define MOTOR_IN2 11
+
+const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+int dataNumber = 0;             // new for this version
+
 int hall_effect_sensor = A0; 
-int hall_effect_val;    
+int hall_effect_val; 
+
 
 void setup() {
 
   pinMode(A0, INPUT);   // set analog pin as input
   Serial.begin(9600);   // initialize serial interface
+  
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
+  
+  digitalWrite(MOTOR_IN1, LOW);
+  analogWrite(MOTOR_IN2, dataNumber);
+
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(MOTOR_IN2, i);
+    hall_effect_val = analogRead(hall_effect_sensor);   // read sensor value
+    Serial.println(hall_effect_val); 
+    delay(500);
+    // Serial.println((int)i);
+  }
+
+  
+     
    
 
 }
 
 void loop() {
-  
-  hall_effect_val = analogRead(hall_effect_sensor);   // read sensor value
-  Serial.println(hall_effect_val);                                // print value to serial
+  // recvWithEndMarker();
+  // showNewNumber();
 
-  delay(1000);  
+  // for (int i = 0; i <= 255; i++) {
+  //   analogWrite(MOTOR_IN2, i);
+  //   Serial.println((int)i);
+  // }
 
+  // hall_effect_val = analogRead(hall_effect_sensor);   // read sensor value
+  // // Serial.println(hall_effect_val);    
+  // delay(500);                            // print value to serial
+
+
+}
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    if (Serial.available() > 0) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void showNewNumber() {
+    if (newData == true) {
+        dataNumber = 0;             // new for this version
+        dataNumber = atoi(receivedChars);   // new for this version
+
+        digitalWrite(MOTOR_IN1, LOW);
+        analogWrite(MOTOR_IN2, dataNumber);
+
+        // Serial.print("This just in ... ");
+        // Serial.println(receivedChars);
+        Serial.print("Data as Number ... ");    // new for this version
+        Serial.println(dataNumber);     // new for this version
+        newData = false;
+    }
 }
